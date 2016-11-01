22824da726e8e2653562086aa6e48c0c35b2cb6c
Jervis <cuijervis@126.com>
2016-11-02 00:11:17 +0800

    create

---
+++ DesignPattern/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace DesignPattern
+{
+    public class Class1
+    {
+        [Fact]
+        public void TestMehtod()
+        {
+            1.ShouldBe(1);
+        }
+    }
+}
